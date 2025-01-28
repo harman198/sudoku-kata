ce945b1046fa33bf43647174684099495c417d1b
Harman Khangura <hakh@collectionhouse.com.au>
2025-01-28 20:49:10 +1000

    Converted project to use top-level statements

--- SudokuKata/SudokuKata/Program.cs
+++ SudokuKata/SudokuKata/Program.cs
@@ -1,16 +1,8 @@
-﻿using System;
+﻿using SudokuKata;
+using System;
 
-namespace SudokuKata
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            SudokuGame.Play();
+SudokuGame.Play();
 
-            Console.WriteLine();
-            Console.Write("Press ENTER to exit... ");
-            Console.ReadLine();
-        }
-    }
-}
+Console.WriteLine();
+Console.Write("Press ENTER to exit... ");
+Console.ReadLine();
